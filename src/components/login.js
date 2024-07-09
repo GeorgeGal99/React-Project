5eedda4037886b2e1686d178aa93e79a2433e771
GeorgeGal99 <144837092+GeorgeGal99@users.noreply.github.com>
2024-07-09 22:53:55 +0300

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -99,4 +99,97 @@ function Register() {
 }
 
 
-export default Register;
+// export default Register;
+
+
+// MARINA
+
+import { useState } from "react";
+import "./input.css";
+
+function Input() {
+    let regex = /^[a-zA-Z]+$/;
+
+    const [usernameValidationMessage, setUsernameValidationMessage] =
+        useState("");
+    const [emailValidationMessage, setEmailValidationMessage] = useState("");
+    const [passwordValidationMessage, setPasswordValidationMessage] =
+        useState("");
+
+    const [showValidationUsername, setShowValidationMessageUsername] =
+        useState(true);
+    const [showValidationEmail, setShowValidationMessageEmail] = useState(true);
+    const [showValidationPassword, setShowValidationMessagePassword] =
+        useState(true);
+
+    function ValidateUsername(event) {
+        let inputData = event.target.value;
+        if (inputData === "") {
+            setUsernameValidationMessage("Can't be blank!");
+            setShowValidationMessageUsername(true);
+        } else if (!regex.test(inputData)) {
+            setUsernameValidationMessage("Can't contain numbers!");
+            setShowValidationMessageUsername(true);
+        } else {
+            setShowValidationMessageUsername(false);
+        }
+    }
+
+    function ValidateEmail(event) {
+        let inputData = event.target.value;
+        if (inputData === "") {
+            setEmailValidationMessage("Can't be blank!");
+            setShowValidationMessageEmail(true);
+        } else if (!inputData.includes("@")) {
+            setEmailValidationMessage("Wrong email format");
+            setShowValidationMessageEmail(true);
+        } else {
+            setShowValidationMessageEmail(false);
+        }
+    }
+
+    function ValidatePassword(event) {
+        let inputData = event.target.value;
+        if (inputData === "") {
+            setPasswordValidationMessage("Can't be blank!");
+            setShowValidationMessagePassword(true);
+        } else {
+            setShowValidationMessagePassword(false);
+        }
+    }
+
+    function Submit() {
+        console.log("Te rog mergi :(");
+    }
+    return (
+        <>
+            <h1>Login</h1>
+            <div>
+                <p>Username</p>
+                <input type="text" onChange={ValidateUsername} />
+                <p>{showValidationUsername ? usernameValidationMessage : ""}</p>
+            </div>
+            <div>
+                <p>Email</p>
+                <input type="text" onChange={ValidateEmail} />
+                <p>{showValidationEmail ? emailValidationMessage : ""}</p>
+            </div>
+            <div>
+                <p>Password</p>
+                <input type="text" onChange={ValidatePassword} />
+                <p>{showValidationPassword ? passwordValidationMessage : ""}</p>
+            </div>
+            <div>
+                <button disabled={!showValidationPassword && !showValidationEmail && !showValidationUsername ? false : true} onClick={Submit}>
+                    Submit
+                </button>
+                {/* <button className={!showValidationPassword && !showValidationEmail && !showValidationUsername ? "not-disabled" : "disabled"}onClick={Submit}>
+          {!showValidationPassword ? "not-disabled" : "disabled"}
+        </button>
+        <p>{showValidationPassword ? "true" : "false"}</p> */}
+            </div>
+        </>
+    );
+}
+
+export default Input;
